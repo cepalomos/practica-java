ddc8339089d20bff9d852cad6e5e72e4a44b7bd1
George <josejorgemartinez1999.jm@gmail.com>
2023-09-06 16:42:21 -0500

    Modelos terminados, y clase para conectar a la base de datos

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,14 @@
+
+package test;
+
+/**
+ *
+ * @author Scorpion
+ */
+public class Test {
+    private static final String SQL_SELECT = "SELECT nombre, profesor, estudiantes, curso FROM asignatura";
+    
+    
+    
+
+}
